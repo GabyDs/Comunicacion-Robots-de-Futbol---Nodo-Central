65f77f5de88bbab69e2030c5ea564d321f0b6844
GabyDs <gabydsch2001@gmail.com>
2025-08-20 16:34:03 -0300

    Created initial wifi network

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,17 @@
 #include <Arduino.h>
+#include <WiFi.h>
 
 // put function declarations here:
 
+#define LED 33
+
+const char* ssid = "esp32wifi";
+const char* password = "12345678";
+
 void setup() {
   // put your setup code here, to run once:
+  WiFi.mode(WIFI_AP);
+  WiFi.softAP(ssid, password);
 }
 
 void loop() {
