73d139be062a7a41f8b99a00c13eacb5ec8af43d
GabyDs <gabydsch2001@gmail.com>
2025-08-21 09:17:08 -0300

    Configured esp32 as AP

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,20 @@
 #include <Arduino.h>
 #include <WiFi.h>
 
-// put function declarations here:
-
-#define LED 33
-
 const char* ssid = "esp32wifi";
 const char* password = "12345678";
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
+  delay(1000);
   WiFi.mode(WIFI_AP);
   WiFi.softAP(ssid, password);
+  Serial.println("AP iniciado. Esperando clientes...");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  int n = WiFi.softAPgetStationNum();
+  Serial.print("Clientes conectados al WiFi: ");
+  Serial.println(n);
+  delay(1000);
 }
-
-// put function definitions here:
